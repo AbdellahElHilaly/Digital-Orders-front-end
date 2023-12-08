588bf0ae34fffb823658808778311b69e184f2bf
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2023-12-08 16:49:21 +0100

    refactoring

---
+++ src/app/core/service/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {User} from "../model/user";
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  url  = 'http://127.0.0.1:8080/api/v1/users';
+
+  async getAllUsers(): Promise<User[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
+  }
+
+
+}
