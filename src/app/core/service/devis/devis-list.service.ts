b574862a73e6e1480199626a273f707436ca1078
marwan-tabib <marwanmaroc297@gmail.com>
2023-12-11 15:20:44 +0100

    marouane | Add the url value and Add getAllDevis method - service/devis/devis-list.service

--- src/app/core/service/devis/devis-list.service.ts
+++ src/app/core/service/devis/devis-list.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient , HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DevisListService {
 
-  constructor() { }
+  url: string = 'http://localhost:8080/api/v1';
+
+  constructor(private http:HttpClient) {}
+
+  getAllDevis(): any{
+    return this.http.get(this.url + '/devis/devis');
+  }
 }
